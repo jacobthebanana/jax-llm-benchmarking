9aac3ddd43a376a44e648de86a3e3439d4cf7dbf
jacobthebanana <jacob@banana.abay.cf>
2023-01-06 23:03:00 +0000

    Revised wandb integration for sweeps on pod slices.

--- launch_run_pod.sh
+++ launch_run_pod.sh
@@ -7,8 +7,11 @@ source ~/.bashrc
 source env/bin/activate
 " > _launch_run_pod.sh 
 
-export |grep WANDB_SWEEP >> _launch_run_pod.sh 
+export |grep WANDB_DIR >> _launch_run_pod.sh 
+export |grep WANDB_ENTITY >> _launch_run_pod.sh 
+export |grep WANDB_PROJECT >> _launch_run_pod.sh 
 export |grep WANDB_RUN >> _launch_run_pod.sh 
+export |grep WANDB_SWEEP >> _launch_run_pod.sh 
 
 echo "python3 run_clm_flax.py --hostname tpu-pod-slice-vm-${TPU_NAME} $@" >> _launch_run_pod.sh
 
